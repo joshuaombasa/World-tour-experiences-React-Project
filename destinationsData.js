66e838f9ff939727b1207e69874c4d59ede705fd
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-24 14:58:14 +0300

    added script file

---
+++ destinationsData.js
@@ -0,0 +1,31 @@
+const destinationsData =  [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1933&q=80",
+        startDate: "12 Jan, 2022",
+        endDate: "24 Jan, 2022",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        title: "Geirangerfkord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2023",
+        endDate: "24 Jan, 2023",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    }
+]
+
+export {destinationsData}
