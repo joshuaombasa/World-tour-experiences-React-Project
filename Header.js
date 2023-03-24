d67abffe22d8158d5996e9e1e8dc1b0a354bc95a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-24 14:58:27 +0300

    added script file

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    
+    return (
+        <div className="header">
+            <div className="header-inner-container">
+                <i class="fa-solid fa-globe"></i>
+                <h4>my travel journal</h4>
+            </div>
+        </div>
+    )
+}
